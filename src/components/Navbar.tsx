2e58bec7430e2141ff44d54e0ccee228e312bbdf
Nicolas Lopez <niclopezt@gmail.com>
2025-06-18 21:31:59 +0700

    add basic search bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,11 +12,11 @@ const Navbar = () => {
         fill="none"
         className="fill-white"
       >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
           {" "}
